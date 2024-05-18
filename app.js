26167a107ee543f41a72fb3ec3f1d540039842da
Lichatti <lisandropiatti@gmail.com>
2024-05-18 15:22:38 +0200

    update domain name generator

--- app.js
+++ app.js
@@ -1,54 +1,33 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon', 'Domain Hack'];
-let domain = [".com", ".net", ".us", ".io"];
-
-let allArrays = [pronoun, adj, noun, domain];
-
-console.log("todo ok");
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon',];
+const domain = [".com", ".net", ".us", ".io", "domainHack"];
 
 
-
-
-
-let generateNameDomain = () => {
+const generateNameDomain = () => {
     
     let domainName = [];
-    
-    for (arr of allArrays) {
-        //  creando un numero random, teniendo en cuenta la extension del array dado
-        let getRandomNumber = () => Math.floor(Math.random() * arr.length);
-        let randomNumber = getRandomNumber();
 
-        // verificando si es un "Domain Hack"
-        if (arr === noun && randomNumber === 2){
-            // eligiendo un item al azar del array "noun"
-            let randomNumberChoice = Math.floor(Math.random() * 2);
-            let stringName = noun[randomNumberChoice];
-
-            // convirtiendo string a un array
-            let arrName = [];
-            for (letter of stringName) {
-                arrName.push(letter);
+    for (itemPronoun of pronoun){
+        for (itemAdj of adj){
+            for (itemNoun of noun) {
+                for (itemDomain of domain) {
+                    if (itemDomain === "domainHack") {
+                        let domainHack = itemNoun.slice(0, -2) +"."+itemNoun.slice(-2);
+                        domainName.push(itemPronoun+itemAdj+domainHack);    
+                    }else {
+                        domainName.push(itemPronoun+itemAdj+itemNoun+itemDomain);
+                    }
+                    
+                    
+                }
             }
-            
-            //agregando el punto en el nombre
-            arrName.splice(-2, 0, '.');
-
-            let finalName = arrName.join("");
-
-            domainName.push(finalName);
-            
-            break;
         }
-
-        //agregando un elmento a nuestro nombre de dominio
-        domainName.push(arr[randomNumber]);
-        console.log(domainName);
     }
 
-    console.log(domainName.join(""));
-    return domainName.join("");
+    console.log(domainName);
+    
+    return domainName;
 }
 
 generateNameDomain();
